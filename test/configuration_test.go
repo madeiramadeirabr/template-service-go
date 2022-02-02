b74678dd2c27a16522cc2e18e790d050de88852c
GuilhermeMLS <guitaikion@gmail.com>
2022-02-02 20:25:01 -0300

    test: refactor tests to use t.Run to describe the test cases

--- test/configuration_test.go
+++ test/configuration_test.go
@@ -7,49 +7,61 @@ import (
 	"testing"
 )
 
-// TODO: add assertion package and describe tests using t.Run
-func TestGetEnvString(t *testing.T) {
-	err := os.Setenv("TEST_ENV_STRING", "test")
-	utils.Fck(err)
-	value := configuration.GetEnvString("TEST_ENV_STRING")
-	if value != "test" {
-		t.Errorf("Expected value to be 'test', but got '%s'", value)
-	}
-}
+// TODO: add assertion package
+func TestConfiguration(t *testing.T) {
+	t.Run("GetEnvString", func(t *testing.T) {
+		t.Run("It should correctly get an existing string variable from the system env", func(t *testing.T) {
+			err := os.Setenv("TEST_ENV_STRING", "test")
+			utils.Fck(err)
+			value := configuration.GetEnvString("TEST_ENV_STRING")
+			if value != "test" {
+				t.Errorf("Expected value to be 'test', but got '%s'", value)
+			}
+		})
+	})
 
-func TestGetEnvBool(t *testing.T) {
-	err := os.Setenv("TEST_ENV_BOOL", "true")
-	utils.Fck(err)
-	value := configuration.GetEnvBool("TEST_ENV_BOOL")
-	if value != true {
-		t.Errorf("Expected value to be 'true', but got '%t'", value)
-	}
-}
+	t.Run("GetEnvString", func(t *testing.T) {
+		t.Run("It should correctly get an existing boolean variable from the system env", func(t *testing.T) {
+			err := os.Setenv("TEST_ENV_BOOL", "true")
+			utils.Fck(err)
+			value := configuration.GetEnvBool("TEST_ENV_BOOL")
+			if value != true {
+				t.Errorf("Expected value to be 'true', but got '%t'", value)
+			}
+		})
+	})
 
-func TestLoadEnvWithNoError(t *testing.T) {
-	err := os.Setenv("PORT", "8080")
-	utils.Fck(err)
-	err = os.Setenv("DEV_ENV", "true")
-	utils.Fck(err)
-	config, err := configuration.LoadConfig()
-	if err != nil {
-		t.Errorf("Expected no error, but got '%s'", err)
-	}
-	if config.Port != "8080" {
-		t.Errorf("Expected value to be '8080', but got '%s'", config.Port)
-	}
-	if config.Dev != true {
-		t.Errorf("Expected value to be 'true', but got '%t'", config.Dev)
-	}
-}
+	t.Run("GetEnvString", func(t *testing.T) {
+		err := os.Setenv("PORT", "8080")
+		utils.Fck(err)
+		err = os.Setenv("DEV_ENV", "true")
+		utils.Fck(err)
+		config, err := configuration.LoadConfig()
 
-func TestLoadEnvWithMissingPort(t *testing.T) {
-	envErr := os.Unsetenv("PORT")
-	utils.Fck(envErr)
-	envErr = os.Setenv("DEV_ENV", "true")
-	utils.Fck(envErr)
-	_, configErr := configuration.LoadConfig()
-	if configErr == nil {
-		t.Errorf("Expected error, but got no error")
-	}
+		t.Run("It should return load config variables without errors", func(t *testing.T) {
+			if err != nil {
+				t.Errorf("Expected no error, but got '%s'", err)
+			}
+		})
+
+		t.Run("It should return a config struct with all mandatory variables correctly filled", func(t *testing.T) {
+			if config.Port != "8080" {
+				t.Errorf("Expected value to be '8080', but got '%s'", config.Port)
+			}
+			if config.Dev != true {
+				t.Errorf("Expected value to be 'true', but got '%t'", config.Dev)
+			}
+		})
+
+		t.Run("It should return an error when there's a missing required variable", func(t *testing.T) {
+			envErr := os.Unsetenv("PORT")
+			utils.Fck(envErr)
+			envErr = os.Setenv("DEV_ENV", "true")
+			utils.Fck(envErr)
+			_, configErr := configuration.LoadConfig()
+			if configErr == nil {
+				t.Errorf("Expected error, but got no error")
+			}
+		})
+	})
 }
