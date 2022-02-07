160b72f932cccc8b79d6360ba1a482a26629bbf6
Ranieri Junior <ran-junior@hotmail.com>
2022-02-07 17:07:08 -0300

    doc: change test description

--- internal/health-check/handlers/health_check_handler_test.go
+++ internal/health-check/handlers/health_check_handler_test.go
@@ -9,12 +9,13 @@ import (
 )
 
 func TestHealthCheck(t *testing.T) {
-
-	t.Run("It should match status message", func(t *testing.T) {
-		status := healthCheckHandler.GetStatus()
-		expected := map[string]interface{}{
-			"status": "OK",
-		}
-		assert.Equal(t, status, expected)
+	t.Run("GetStatus", func(t *testing.T) {
+		t.Run("It should match status message", func(t *testing.T) {
+			status := healthCheckHandler.GetStatus()
+			expected := map[string]interface{}{
+				"status": "OK",
+			}
+			assert.Equal(t, status, expected)
+		})
 	})
 }
--- internal/health-check/routes/health_check_router_test.go
+++ internal/health-check/routes/health_check_router_test.go
@@ -15,9 +15,11 @@ func TestHealthCheckRouter(t *testing.T) {
 	app := fiber.New()
 	healthCheckRouter.RegisterRoutes(app)
 
-	t.Run("It should return status 200", func(t *testing.T) {
-		resp, err := app.Test(httptest.NewRequest("GET", "/health-check", nil))
-		assert.Nil(t, err, fmt.Sprintf("Expected no error, but got '%s'", err))
-		assert.Equal(t, resp.StatusCode, 200, "Expected status code 200, but got %d", resp.StatusCode)
+	t.Run("Health check handler", func(t *testing.T) {
+		t.Run("It should return status 200", func(t *testing.T) {
+			resp, err := app.Test(httptest.NewRequest("GET", "/health-check", nil))
+			assert.Nil(t, err, fmt.Sprintf("Expected no error, but got '%s'", err))
+			assert.Equal(t, resp.StatusCode, 200, "Expected status code 200, but got %d", resp.StatusCode)
+		})
 	})
 }
