fdb40c24fea3f4108009b440d68a19c6ca7424f1
Ranieri <ran-junior@hotmail.com>
2022-02-09 14:19:00 -0300

    feat: added router error handling (#18)

--- cmd/go_service_template/main.go
+++ cmd/go_service_template/main.go
@@ -9,17 +9,26 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/recover"
 )
 
 func main() {
 	config, err := configuration.Load()
 	utils.Fck(err)
+	isDevelopment := config.IsDevelopmentEnvironment()
+
 	logger := Logger.New(
 		config.ServiceName,
 		utils.Clock{},
-		config.IsDevelopmentEnvironment(),
+		isDevelopment,
 	)
 	app := fiber.New()
+ 
+	ErrorHandlerConfig := recover.Config{
+		EnableStackTrace: isDevelopment,
+	}
+	app.Use(recover.New(ErrorHandlerConfig))
+
 	healthcheckrouter.RegisterRoutes(app, logger, config)
 	logger.Info("\"🧜‍ Core APIs Go Service Template Listening on port: " + config.Port)
 	log.Fatal(app.Listen(":" + config.Port))
